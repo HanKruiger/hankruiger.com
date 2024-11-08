c651cdb69f9ff95d48c4d55a1d8d93d798a3059e
Han Kruiger <HanKruiger@users.noreply.github.com>
2024-11-08 09:50:27 +0000

    migrate to nuxt 4 compatibility version & new directory structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ if (umamiWebsiteId) {
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-10-25',
   modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/tailwindcss'],
 
