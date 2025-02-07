c49e9702d5520a0490cd25cd077522951e7cf62e
Han Kruiger <HanKruiger@users.noreply.github.com>
2025-02-07 09:32:40 +0100

    add post about footnotes

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-full">
     <AppHeader/>
-    <main class="flex-[1_0_auto] py-4 px-4 sm:px-0 mx-auto max-w-sm md:max-w-xl lg:max-w-2xl">
+    <main class="flex-[1_0_auto] py-6 px-4 sm:px-0 mx-auto max-w-sm md:max-w-xl lg:max-w-2xl">
       <slot/>
     </main>
     <AppFooter/>
