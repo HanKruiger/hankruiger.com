ada8973b8328e5f85610cdde28134ee6b597b3e6
Han Kruiger <HanKruiger@users.noreply.github.com>
2025-02-07 15:25:06 +0100

    add silly error page

---
+++ app/error.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{ error: NuxtError }>();
+
+type StatusCodeAnimal = 'cat' | 'dog';
+
+const preferredAnimal = ref<StatusCodeAnimal>('cat')
+
+const otherAnimal = computed(() => {
+  if (preferredAnimal.value === 'cat') {
+    return 'dog';
+  } else {
+    return 'cat';
+  }
+});
+
+const toggle = () => {
+  preferredAnimal.value = otherAnimal.value;
+}
+
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="w-full h-full flex flex-col justify-center items-center gap-4">
+      <img :src="`https://http.${preferredAnimal}/${error.statusCode}.jpg`">
+      <UButton @click="toggle">I prefer {{ otherAnimal }}s</UButton>
+    </div>
+  </NuxtLayout>
+</template>
